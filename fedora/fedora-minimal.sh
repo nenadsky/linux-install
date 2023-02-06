ddad7ad5e812ddff0089f1e1a84d79030002b61d
Nenad Dimitrovski <nenad.dimitrovski@gmail.com>
2023-02-06 19:06:44 +0100

    Create fedora-minimal.sh

---
+++ fedora/fedora-minimal.sh
@@ -0,0 +1,35 @@
+#!/bin/env bash
+##### CHECK FOR SUDO or ROOT ##################################
+if ! [ $(id -u) = 0 ]; then
+  echo "This script must be run as sudo or root, try again..."
+  exit 1
+fi
+
+# Install KDE Packages
+dnf install \
+  xorg-x11-server-Xorg \
+  xorg-x11-drv-amdgpu \
+  xorg-x11-drv-fbdev \
+  xorg-x11-drv-evdev \
+  sddm plasma-desktop \
+  plasma-discover \
+  plasma-systemmonitor \
+  sddm-kcm \
+  kde-partitionmanager \
+  cups \
+  kde-print-manager \
+  dolphin \
+  konsole5 \
+  okular \
+  gwenview \
+  spectacle \
+  kate \
+  ark \
+  fuse \
+  bash-completion \
+  htop \
+  neofetch
+  
+systemctl enable sddm
+systemctl set-default graphical.target
+  
