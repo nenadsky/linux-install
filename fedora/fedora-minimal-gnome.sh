b923ecee2c7c87b42de3d94d0f0cf67add0d280f
Nenad Dimitrovski <nenad.dimitrovski@gmail.com>
2024-11-12 19:20:50 +0100

    Create fedora-minimal-gnome.sh

---
+++ fedora/fedora-minimal-gnome.sh
@@ -0,0 +1,33 @@
+#!/bin/env bash
+##### CHECK FOR SUDO or ROOT ##################################
+if ! [ $(id -u) = 0 ]; then
+  echo "This script must be run as sudo or root, try again..."
+  exit 1
+fi
+
+# Install Gnome Packages
+dnf install \
+  gdm \
+  gnome-shell \
+  gnome-console \
+  nautilus \
+  gnome-calculator \
+  gnome-disk-utility \
+  gnome-system-monitor \
+  cups \
+  gnome-software \
+  gnome-tweaks \
+  evince \
+  gedit \
+  file-roller \
+  firefox \
+  fuse \
+  bash-completion \
+  htop \
+  libreoffice \
+  libreoffice-kf6 \
+  fastfetch \
+  @development-tools
+  
+systemctl enable gdm.service
+systemctl set-default graphical.target
